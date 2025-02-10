7bb97a0cf0ada92c93ce44da8d4766fa54f0bfae
fabroche <kraxusmmo@gmail.com>
2025-02-10 23:24:13 +0000

    feat: Description edited

--- src/api/images.js
+++ src/api/images.js
@@ -4,31 +4,31 @@ export const images = [
     {
         name: "Regalito",
         url: `${BASE_IMAGES_DIR}/aby-regalito.jpg`,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.',
+        description: 'Psss oye tú, sí tú misma, mira esto!!!😏 "...como cuando pones cara de boba porque tu negro te da tu primer presentico..."🫠',
         emoji: "🎁"
     },
     {
         name: "Viajesito",
         url: `${BASE_IMAGES_DIR}/aby-yo-pe.jpg`,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.',
+        description: '...Y dime algo de ese primer viajesito, Uff!! donde se te olvidó el mundo y hasta el lugar del universo en que vivías.😜',
         emoji: "🚎"
     },
     {
         name: "Besito",
         url: `${BASE_IMAGES_DIR}/aby-yo-pe-besito.jpg`,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.',
+        description: '...O de ese momento donde el tiempo se friseaba de forma tal que ni siquiera conseguías escuchar tu propia respiración.🥵',
         emoji: "😈"
     },
     {
         name: "Fotico",
         url: `${BASE_IMAGES_DIR}/aby-yo-pe-2.jpg`,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.',
+        description: 'Ohh!! nono pera otra mejor aún... recuerdas esa sensación de paz total, calma y equilibrio cuando te acercabas a tu prietico.🎇',
         emoji: "📸"
     },
     {
         name: "Manitos",
         url: `${BASE_IMAGES_DIR}/aby-yo-pe-manitos.jpg`,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.',
+        description: '...Finalmente ese momentico cuando te agarra de la manito y sientes que puedes sobrevolar una cascada, nadar en el cielo o volar en el océano.❤️',
         emoji: "😘"
     },
 ]
